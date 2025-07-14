2c989917a24cce6af9309b99bcc0b894ee416ffc
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-07-14 12:01:08 -0400

    Refactor to process multiple frames and output as JSON

--- src/main.ts
+++ src/main.ts
@@ -1,91 +1,74 @@
+import fs from "node:fs/promises";
+import path from "node:path";
 import sharp from "sharp";
 import { mapValue } from "./utils.ts";
 
-const DENSITY = "@#%$=*xo~.";
+// exit early if no path was passed
+if (!process.argv[2]) {
+    console.error("No path provided");
+    process.exit(1);
+}
 
-// extract raw, unsigned 8-bit RGBA pixel data from png
-const testImg = await sharp("public/test.png")
-    .raw()
-    .toBuffer({ resolveWithObject: true });
+type SharpImg = {
+    data: Buffer;
+    info: sharp.OutputInfo;
+};
 
-// string to store the image in ascii format
-let asciiImageStr = "";
-// start at the top row and then check each column in the row so
-// the asciiImageStr is built top to bottom, left to right.
-for (let row = 0; row < testImg.info.height; row++) {
-    for (let col = 0; col < testImg.info.width; col++) {
-        // formula to find the first channels value for a pixel.
-        let pixelIndex = (row * testImg.info.width + col) * 4;
-        // the channels are red, green, blue, and alpha
-        let r: number = testImg.data[pixelIndex];
-        let g: number = testImg.data[pixelIndex + 1];
-        let b: number = testImg.data[pixelIndex + 2];
-        let a: number = testImg.data[pixelIndex + 3];
+const DENSITY = "#$?0=*c~. ";
+const FRAMES_DIR = path.resolve(process.argv[2]);
 
-        if (a === 0) {
-            asciiImageStr += " ";
-        } else {
-            // calculate the luminance of a given pixel
+// load all the files
+const frameFiles = await fs
+    .readdir(FRAMES_DIR)
+    .then((res) => res.sort())
+    .catch((err) => {
+        throw new Error(err);
+    });
+
+// process each frame file
+let asciiFrames: string[][] = [];
+for (let i = 0; i < frameFiles.length; i++) {
+    // extract raw, unsigned 8-bit RGB pixel data from png
+    const data = await sharp(path.resolve(FRAMES_DIR, frameFiles[i]))
+        .raw()
+        .toBuffer({ resolveWithObject: true });
+
+    // create an array to store each frame
+    asciiFrames[i] = constructFrame(data);
+}
+
+function constructFrame(sharpImg: SharpImg): string[] {
+    // string to store the image in ascii format
+    let asciiImage: string[] = Array(sharpImg.info.height).fill("");
+    // start at the top row and then check each column in the row so
+    // the asciiImageStr is built top to bottom, left to right.
+    for (let row = 0; row < sharpImg.info.height; row++) {
+        for (let col = 0; col < sharpImg.info.width; col++) {
+            // formula to find the first channels value for a pixel.
+            let pixelIndex =
+                (row * sharpImg.info.width + col) * sharpImg.info.channels;
+
+            // the channels are red, green, blue
+            let r, g, b;
+            r = sharpImg.data[pixelIndex];
+            g = sharpImg.data[pixelIndex + 1];
+            b = sharpImg.data[pixelIndex + 2];
+
+            // calculate the luminance of a given pixel (rec 709)
             // more info: https://en.wikipedia.org/wiki/Relative_luminance
             let luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b;
-            // map the brightness to a character density index
+
+            // map the luminance to a character density index
             let charIndex = Math.round(
                 mapValue(luminance, 0, 255, 0, DENSITY.length - 1),
             );
+
             // add a character (pixel) to the row
-            asciiImageStr += DENSITY.charAt(charIndex);
+            asciiImage[row] += DENSITY.charAt(charIndex);
         }
     }
-    asciiImageStr += "\n";
+    return asciiImage;
 }
 
-// // frame file naming convention "frame_0001.txt" where
-// // the first frame is 0001
-// const framesDir = "./frames";
-
-// fs.readdir(framesDir, { encoding: "utf-8" }, (err, frameFiles) => {
-//     // stop on any errors
-//     if (err) {
-//         console.error(err);
-//         return;
-//     }
-
-//     // make sure the array of frame files is in order
-//     frameFiles.sort();
-
-//     // create an array to store each frame
-//     const frameLines = [];
-
-//     // count how many frames have been processed so we can
-//     // create the file on the last readline file close.
-//     let frameFilesProcessed = 0;
-
-//     for (let i = 0; i < frameFiles.length; i++) {
-//         // store the lines of a frame in an array
-//         frameLines[i] = [];
-
-//         // create a stream to read each line of a file
-//         const rl = readline.createInterface({
-//             input: fs.createReadStream(path.join(framesDir, frameFiles[i])),
-//             crlfDelay: Infinity,
-//         });
-
-//         // read each line
-//         rl.on("line", (line) => {
-//             frameLines[i].push(line);
-//         });
-
-//         rl.on("close", () => {
-//             frameFilesProcessed++;
-//             // check if all the files have been processed
-//             if (frameFilesProcessed === frameFiles.length) {
-//                 let frameData = JSON.stringify(frameLines);
-//                 // all files have been processed so write to an output file
-//                 fs.writeFile("frames.json", frameData, (err) => {
-//                     if (err) throw err;
-//                     console.log("The file has been saved!");
-//                 });
-//             }
-//         });
-//     }
-// });
+// // all files have been processed so write to an output file
+fs.writeFile("frames.json", JSON.stringify(asciiFrames));
