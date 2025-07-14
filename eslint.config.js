079a43e2410569a101e6532995dccd9a4445620c
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-07-14 17:08:12 -0400

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,15 @@
 // @ts-check
 
 import eslint from "@eslint/js";
+import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
     eslint.configs.recommended,
     tseslint.configs.strict,
-    tseslint.configs.stylistic,
 );
