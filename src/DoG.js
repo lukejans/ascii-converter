9936dc5452afad0a7541895835a3af59ed7648c9
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-07-17 16:15:25 -0400

    Add difference of gaussians preprocessing step
    Note that this isn't being used at the moment becuase for the current
    use case it makes the images look worse as it is simple clip art.

---
+++ src/DoG.js
@@ -0,0 +1,28 @@
+import fs from "fs/promises";
+import sharp from "sharp";
+
+const buffer = await fs.readFile("testing/joystick.png");
+
+// apply the difference of gaussians
+const diffOfGaussians = await sharp(buffer)
+    .normalise()
+    .blur(1)
+    .greyscale()
+    .composite([
+        {
+            input: await sharp(buffer)
+                .normalise()
+                .blur(2)
+                .greyscale()
+                .toBuffer(),
+            blend: "difference",
+        },
+    ])
+    .toBuffer();
+
+let res = await sharp(diffOfGaussians)
+    .threshold(1)
+    .removeAlpha()
+    .toBuffer({ resolveWithObject: true });
+
+console.log(res.info);
