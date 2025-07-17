2041a1c472fbcaecb55c5a7eab03aa9b5328629e
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-07-17 10:07:11 -0400

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,11 +5,17 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
     {
         languageOptions: {
             globals: globals.node,
         },
+
+        rules: {
+            // Note: you must disable the base rule as it can report incorrect errors
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "warn",
+        },
     },
-    eslint.configs.recommended,
-    tseslint.configs.strict,
 );
