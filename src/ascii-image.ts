77bac139a7eeeebd7ed091e7e78e4b49bd363644
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2025-08-05 00:53:49 -0400

    converter: move char mapping helper functions back to ascii-image.ts

--- src/ascii-image.ts
+++ src/ascii-image.ts
@@ -1,6 +1,7 @@
 import sharp from "sharp";
-import type { ImgModifications } from "./types/image.ts";
-import { edgeToChar, lumaToChar } from "./utils/char.ts";
+import config from "./config.ts";
+import type { ImgModifications } from "./types/image.types.ts";
+import { mapValue } from "./utils/math.ts";
 
 export default class AsciiImg {
     /**
@@ -233,4 +234,44 @@ export default class AsciiImg {
     get text(): string[] {
         return this.#text;
     }
+}
+
+function edgeToChar(angle: number) {
+    let edgeChar: string = "";
+
+    if ((angle >= 0 && angle <= 19) || (angle <= 180 && angle >= 161)) {
+        // 38º of range
+        edgeChar = "-";
+    } else if (angle >= 20 && angle <= 70) {
+        // 50º of range
+        edgeChar = "\\";
+    } else if (angle >= 71 && angle <= 109) {
+        // 38º of range
+        edgeChar = "|";
+    } else if (angle >= 110 && angle <= 160) {
+        // 50º of range
+        edgeChar = "/";
+    } else {
+        // TODO: this currently will never trigger but in the future
+        //       try to find a method to detect corner chars. But this
+        //       might only be possible in low detail images such as
+        //       clip art.
+        edgeChar = "+";
+    }
+
+    return edgeChar;
+}
+
+function lumaToChar(luminance: number) {
+    // the characters being used to represent the luma in an image
+    // which is a practical measurement of a pixels brightness. This
+    // charset is listed from darkest to brightest which is currently
+    // being represented as most to least dense.
+
+    // map the luma to a character
+    const index = Math.floor(
+        mapValue(luminance, 0, 255, 0, config.chars.luma.length - 1),
+    );
+
+    return config.chars.luma[index];
 }
